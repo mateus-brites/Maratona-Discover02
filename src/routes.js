f311ac12c4be043a6caa0a3632ccfd7483918156
mateus <mateusmmo15@gmail.com>
2021-05-21 19:54:18 -0300

    aula 02

---
+++ src/routes.js
@@ -0,0 +1,192 @@
+const { request } = require("express");
+const express = require("express");
+const routes = express.Router();
+
+const views = __dirname + "/views/";
+
+const Profile = {
+    data: {
+        name: "mateus",
+        avatar: "https://github.com/mateus-brites.png",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 75
+    },
+
+    controllers: {
+        index(req, res) {
+            return res.render(views + "profile", { profile: Profile.data })
+        },
+        update(req, res) {
+            const data = req.body;
+    
+            const weeksPerYear = 52;
+    
+            const weeksPerMounth = (weeksPerYear - data["vacation-per-year"])/12;
+    
+            const weeksTotalHours = data["days-per-week"] * data["hours-per-day"];
+    
+            const monthlyTotalHours = weeksTotalHours * weeksPerMounth;
+    
+            data['value-hour'] = data['monthly-budget']/monthlyTotalHours;
+    
+            Profile.data = data;
+    
+            return res.redirect('/profile');
+        }
+    }
+}
+
+
+const Job = {
+    data: [
+        {
+            id: 1,
+            name: "Pizzaria Guloso",
+            "daily-hours": 2,
+            "total-hours": 1,
+            created_at: Date.now(),
+            budget: 4500
+        },
+        {
+            id: 2,
+            name: "OneTwo Projects",
+            "daily-hours": 3,
+            "total-hours": 47,
+            created_at: Date.now(),
+            budget: 4500
+        }
+    ],
+    controllers: {
+        index(req, res) {
+            const updatedJobs = Job.data.map((job) => {
+                const remaining = Job.services.remainingDays(job);
+        
+                const status = remaining <= 0 ? "done" : "progress";
+
+        
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
+                    //budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
+                }
+            })
+        
+            return res.render(views + "index", { jobs: updatedJobs})
+        },
+        create(req, res){
+            return res.render(views + "job");
+        },
+        save(req, res) {
+            const lastId = Job.data[Job.data.length -1]?.id || 0;
+            Job.data.push({
+                id: lastId +1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                created_at: Date.now()
+            }
+            );
+
+            return res.redirect('/');
+        }, 
+        show(req, res){
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+
+            if(!job){
+                return res.send("Job not found");
+            }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"]);
+
+            return res.render(views + "job-edit", { job });
+        },
+        update(req, res) {
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+
+            if(!job){
+                return res.send("Job not found");
+            }
+
+            const updatedJobs = {
+                ...job,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"]
+            }
+
+            Job.data = Job.data.map( job => {
+                if(Number(job.id) === Number(jobId)){
+                    job = updatedJobs;
+                }
+                return job;
+            });
+
+            res.redirect("/job/" + jobId);
+        },
+        delete(req, res) {
+            const jobId = req.params.id;
+
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId));
+
+            return res.redirect("/")
+        }
+    },
+    services: {
+        remainingDays(job) {
+            // Quantos dias necessários para executar o projeto
+            const remainingDay = (job["total-hours"] / job["daily-hours"]).toFixed();
+    
+            // Data que marca o início do projeto
+            const createdDate = new Date(job.created_at);
+    
+            // Data que marca o fim do projeto
+            const dueDay = createdDate.getDate() + Number(remainingDay);
+    
+            // Transforma o dueDay em um Date
+            const dueDateInMs = createdDate.setDate(dueDay);
+    
+            // Quantos dias faltam para o fim do projeto em mili segundos
+            const timeDiffInMs = dueDateInMs - Date.now();
+    
+            // Mili segundos existentes em 1 dia
+            const dayInMs = 1000*3600*24;
+    
+            // Quantos dias faltam para o fim do projeto em dias
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+        
+            return dayDiff
+        },
+        calculateBudget: (job, valueHour) => job["total-hours"] * valueHour
+    }
+}
+
+
+
+routes.get('/', Job.controllers.index);
+
+routes.get('/job', Job.controllers.create);
+
+routes.post('/job', Job.controllers.save);
+
+routes.get('/job/:id', Job.controllers.show);
+
+routes.post('/job/:id', Job.controllers.update);
+
+routes.post('/job/delete/:id', Job.controllers.delete);
+
+routes.get('/profile', Profile.controllers.index);
+
+routes.post('/profile', Profile.controllers.update);
+
+//console.log(__dirname);
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+server.set('view engine', 'ejs');
+
+server.use(express.static("public"));
+
+server.use(express.urlencoded());
+
+server.use(routes);
+
+
+server.listen(3000, () => console.log("server rodando"));
